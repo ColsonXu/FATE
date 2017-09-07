8b3f33be11bc019b2c9c1e900d53aafed0388bce
Colson Xu <colson.xu@gmail.com>
2017-09-06 18:24:55 -0700

    Resolved conflict.

--- FATE_Array_VIS_FOR_TK.py
+++ FATE_Array_VIS_FOR_TK.py
@@ -4,7 +4,7 @@ Version of Aug. 5, 2017.
 '''
 
 from show_state_array import initialize_tk, state_array, state_display, STATE_WINDOW, test
-import Missionaries
+import FATE
 from tkinter import font
 
 myFont=None
@@ -22,17 +22,17 @@ def render_state(s):
     # and we do it (the first time this method is called).
     global myFont
     if not myFont:
-        myFont = font.Font(family="Helvetica", size=18, weight="bold")
+        myFont = font.Font(family="Helvetica", size=10, weight="bold")
     print("In render_state, state is "+str(s))
     # Create the default array of colors
-    plants = (67,160,71)# 0
-    ice = (245,245,245)#6
-    beef = (194,24,91)# 2
-    ocean = (2,136,209)# 7
-    empty = (161,136,127)# 1
-    house = (253,216,53)# 5
-    power = (96,125,139)# 4
-    mine = (0,0,0)# 3
+    plants = (67,160,71)    # 0
+    ice = (245,245,245)     # 6
+    beef = (194,24,91)      # 2
+    ocean = (2,136,209)     # 7
+    empty = (161,136,127)   # 1
+    house = (253,216,53)    # 5
+    power = (96,125,139)    # 4
+    mine = (0,0,0)          # 3
 
     translate = {plants: 'Tree', ice: 'Ice', beef: 'Cattles', ocean: 'Ocean',\
                  empty: 'Empty', house: 'House', power: 'Industry', mine: 'Mining'}
@@ -45,13 +45,13 @@ def render_state(s):
     
     for i in range(10):
       for j in range(10):
-        the_color_array[i][j] = color_dic[the_color_array[i][j]]
+        the_color_array[i][j] = color_dic[s['board'][i][j]]
         the_string_array[i][j] = translate[the_color_array[i][j]]
 
     
     
     
-    caption="Current state of the puzzle. Textual version: "+Missionaries.describe_state(s)
+    caption="Current state of the puzzle. Textual version: " + FATE.describe_state(s)
     the_state_array = state_array(color_array=the_color_array,
                                   string_array=the_string_array,
                                   text_font=myFont,

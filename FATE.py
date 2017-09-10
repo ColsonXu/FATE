6524860d9703548e6d4abc03282af21aeb91c195
Leo <Leo3418@users.noreply.github.com>
2017-09-10 16:56:00 -0700

    Revert "Revert "Change initial game map layout""
    This reverts commit 60ac15d83d9b0dcd40ae717b9f123af74d8bcbc7.

--- FATE.py
+++ FATE.py
@@ -360,9 +360,13 @@ class Operator:
     7: Ocean
     '''
 
-row = [0] * 10
-board = [row[:] for i in range(9)]
-board.append([7, 7, 7, 7, 7, 7, 7, 7, 7, 6])
+row = [7, 0, 0, 0, 0, 0, 0, 0, 0, 7]
+board = [row[:] for i in range(10)]
+board[0] = ([7, 7, 7, 6, 6, 6, 6, 7, 7, 7])
+board[2] = ([7, 0, 0, 0, 6, 7, 7, 0, 0, 7])
+board[3] = ([7, 0, 0, 0, 0, 0, 0, 7, 0, 7])
+board[4] = ([7, 0, 0, 0, 0, 0, 0, 0, 7, 7])
+board[9] = ([7, 7, 7, 6, 6, 6, 6, 7, 7, 7])
 
 initialState = {
                 'p': 100,               # Population
